127c42ea3964c9cd94d416cf8a8b94730a2e8f6f
jc299581 <blake.hobbs@my.jcu.au>
2015-04-22 13:25:29 +1000

    Create artisthome.php

---
+++ artisthome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        
+        <h1>Artist Records</h1>
+        <a href="">Add/Modify records </a>
+        <a href="">View records </a>
+
+    </body>
+       
+</html>
