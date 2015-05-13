b03dc8c0d9f0a622fbf866ecde09fc26e415975c
jc299581 <blake.hobbs@my.jcu.au>
2015-05-13 13:01:21 +1000

    Update events.php

--- events/events.php
+++ events/events.php
@@ -1,5 +1,5 @@
 <?php
-include("dbconnection.php");
+    include("dbconnection.php");
 ?>
 
 <!doctype html>
@@ -20,7 +20,16 @@ include("dbconnection.php");
         </p>
         <p>
             <label for="evn_artist">Artist: </label>
-            <input type="text" name="evn_artist" id="evn_artist">
+            <?php
+                $sql="SELECT art_name,art_id FROM Artists";
+                echo "Line 1";
+                echo "<select name='evn_artist' id='evn_artist'>"; // list box select command
+                foreach ($dbh->query($sql) as $row){//Array or records stored in $row
+                echo "<option value='$row[art_id]'>$row[art_name]</option>"; 
+                /* Option values are added by looping through the array */ 
+                }
+                echo "</select>";// Closing of list box
+            ?>
         </p>
         <p>
             <label for="evn_blurb">Description: </label>
@@ -32,11 +41,14 @@ include("dbconnection.php");
         </p>
         <p>
             <label for="date">date:</label>
-            <input type="text" name="evn_date" id="evn_date">
+            <input type="text" name="evn_day" id="day" placeholder="dd" size="2" maxlength="2">
+            <input type="text" name="evn_month" id="month" placeholder="mm" size="2" maxlength="2">
+            <input type="text" name="evn_year" id="year" placeholder="yyyy" size="4" maxlength="4">
         </p>
         
         <p>
             <input type="submit" name="submit" id="submit" value="Insert Entry">
+            <input type="submit" name="submit" value="Delete Old Entries">
         </p>
     </fieldset>
 </form>
