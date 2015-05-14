854583796f41b10ae6e0d5300e680bb4a367afb0
Tay94 <tayla.black@my.jcu.edu.au>
2015-05-14 17:54:08 +1000

    Create bulletin.php
    draft

---
+++ bulletin/bulletin.php
@@ -0,0 +1,36 @@
+<?php
+include("dbconnection.php");
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP SQLite Music Centre Database</title>
+</head>
+
+<body>
+<h1> Bulletin</h1>
+<form id="insert" name="insert" method="post" action="dbProcessMember.php">
+    <fieldset class="subtleSet">
+        <h2>Insert new item:</h2>
+        <p>
+            <label for="bul_title">Title: </label>
+            <input type="text" name="bul_title" id="bul_title">
+        </p>
+        <p>
+            <label for="<bul_des">Description: </label>
+            <textarea cols="50" rows="3" name="bul_des" form="insert"></textarea>
+        </p>
+        <p>
+            <label for="bul_expDate">Expiration date:</label>
+            <input type="text" name="bul_expDate" id="bul_expDate" />
+        </p>
+        <p>
+            <input type="submit" name="submit" id="submit" value="Insert Entry">
+        </p>
+    </fieldset>
+</form>
+
+</body>
+</html>
